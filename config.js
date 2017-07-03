3794e6aa4e6b64cad839d81a51f571dee5195d1f
Christian Auer <cauer@eggs.de>
2017-07-03 12:44:24 +0200

    added dynamic port setting by docker

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const path = require('path')
-console.log(process.env)
+
 module.exports = {
 	"certPath": path.join(__dirname, process.env.CERT),
 	"keyPath": path.join(__dirname, process.env.KEY),
 	"host": process.env.HOST,
 	"port": process.env.PORT_WEB,
 	"dbConn": process.env.DB_CONN,
+	"jwtSecret": process.env.JWT_SECRET,
+	"apiVersion": process.env.API_VERSION
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const app = require('./app')
+const ip = require('ip')
 // exit :: Error -> Unit
 const exit = err => {
-	console.log(err)
+	console.error('ERROR:')
+	console.error(err)
 	process.exit(1)
 }
 // start :: (String, Express) -> Unit
 const start = (key, app) => 
-	console.log(key + ' listens on port: ' + app.address().port)
+	console.log(key + ' listens on: ' + ip.address() + ':' + app.address().port)
 // startServer :: Config -> Unit
 const startServer = (config) => app(config)
 	.fork(
